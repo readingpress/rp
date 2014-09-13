57875c8a708d1d435ed0a2c03158e8adbb20d9da
michaeldewolf85 <michaeldewolf85@gmail.com>
2014-09-13 19:14:31 -0400

    Change default mysql password

--- puppet/modules/mysql/manifests/init.pp
+++ puppet/modules/mysql/manifests/init.pp
@@ -22,7 +22,7 @@ class mysql {
   # We set the root password here
   exec { 'set-mysql-password':
     unless  => 'mysqladmin -uroot -proot status',
-    command => "mysqladmin -uroot password a9120ed2b58af37862a83f5b9f850819ed08b2a9",
+    command => "mysqladmin -uroot password root",
     path    => ['/bin', '/usr/bin'],
     require => Service['mysql'];
   }
