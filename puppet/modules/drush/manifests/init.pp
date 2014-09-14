080f0cb79fa67c27f891a54f2cf4b70f123c02c3
michaeldewolf85 <michaeldewolf85@gmail.com>
2014-09-13 20:00:29 -0400

    Add drush to vagrant

---
+++ puppet/modules/drush/manifests/init.pp
@@ -0,0 +1,30 @@
+class drush {
+
+  include drush::params
+
+  package { 'Console_Table':
+    ensure   => present,
+    provider => pear,
+    require  => Package['php-pear'],
+  }
+
+  exec { 'fetch-drush':
+    cwd     => '/tmp',
+    command => "/usr/bin/git clone --branch $drush::params::branch_name http://git.drupal.org/project/drush.git",
+    creates => '/tmp/drush',
+    require => Package['php5-cli', 'php-pear', 'Console_Table', 'git-core'], 
+  }
+
+  file { '/usr/local/lib/drush':
+    ensure  => directory,
+    recurse => true,
+    purge   => true,
+    source  => '/tmp/drush',
+    require => Exec['fetch-drush'],
+  }
+
+  file { '/usr/local/bin/drush':
+    ensure  => '/usr/local/lib/drush/drush',
+    require => File['/usr/local/lib/drush'],
+  }
+}
