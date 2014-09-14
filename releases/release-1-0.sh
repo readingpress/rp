cd015c74326d230140145de310fe93f581e7b787
michaeldewolf85 <michaeldewolf85@gmail.com>
2014-09-14 14:05:11 -0400

    Add drush install script

---
+++ releases/release-1-0.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Get the start time.
+RELEASE_START=$(date +%s)
+
+# Used for file paths to scripts that need to run for this release.
+RELEASE_TAG=`basename $0`
+RELEASE_TAG="$(RELEASE_TAG%.*}"
+
+drush site-install standard --account-name=rp --account-pass=rp
+drush vset site_name 'Reading Press' -y
+drush vset site_mail 'noreply@readingpress.com' -y
