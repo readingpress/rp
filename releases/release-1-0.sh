8cb6430416f96ff762705846d19c49058ac393ca
michaeldewolf85 <michaeldewolf85@gmail.com>
2014-09-14 14:08:34 -0400

    Make release script executable

--- releases/release-1-0.sh
+++ releases/release-1-0.sh
@@ -5,7 +5,7 @@ RELEASE_START=$(date +%s)
 
 # Used for file paths to scripts that need to run for this release.
 RELEASE_TAG=`basename $0`
-RELEASE_TAG="$(RELEASE_TAG%.*}"
+RELEASE_TAG="${RELEASE_TAG%.*}"
 
 drush site-install standard --account-name=rp --account-pass=rp
 drush vset site_name 'Reading Press' -y
